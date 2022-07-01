9c45847ce7df39eea443fd0071864f65cc279dbd
Laxeder <Laxederbr@gmail.com>
2022-06-30 22:26:24 -0300

    criação do servidor account

---
+++ app.js
@@ -0,0 +1,41 @@
+require("dotenv/config");
+
+const server = require("./src/server");
+const logger = require("./src/infrastructure/config/logger");
+
+//ACC062
+
+server.listen(process.env.PORT, () => {
+  logger.info(`port: ${process.env.PORT}`);
+  logger.info(`author: ${process.env.AUTHOR}`);
+  logger.info(`hostname: ${process.env.HOSTNAME}`);
+  logger.info(`NODE_ENV: ${process.env.NODE_ENV}`);
+});
+
+// criar uma conta
+
+/*
+{
+  "first_name": "John",
+  "last_name": "Doe",
+  "email": "xx@xx.com",
+  "password": "123456",
+  "confirm_password": "123456",
+  "phone": "123456789",
+  "birthdate": "01/01/1990",
+  "cpf": "123456789",
+  "rg": "123456789",
+  "street": "Rua 1",
+  "number": "1",
+  "complement": "Apto 1",
+  "neighborhood": "Bairro 1",
+  "city": "Cidade 1",
+  "state": "SP",
+  "zipcode": "12345678",
+  "nickname": "John Doe",
+  "picture": " ",
+  "description": "description",
+  "profession": "profession",
+  "company": "company",
+} 
+*/
