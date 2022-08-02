29c2d566bb1a0378bf7cd03fa7d4942ea179d19d
Laxeder <Laxederbr@gmail.com>
2022-08-01 21:32:12 -0300

    Atualizando logger

--- src/infrastructure/config/logger.js
+++ src/infrastructure/config/logger.js
@@ -1,7 +1,32 @@
-const pino = require('pino');
-const pretty = require('pino-pretty');
+const winston = require("winston");
 
-const stream = pretty({colorize: true, level: 'debug'});
-const logger = pino(stream);
+// Configura o logger
+let alignColorsAndTime = winston.format.combine(
+  winston.format.colorize({
+    all: true,
+  }),
+  winston.format.timestamp({
+    format: "YY-MM-DD HH:mm:ss",
+  }),
+  winston.format.printf(
+    (info) => `[${info.level}] [${info.timestamp}] ${info.message}`
+  ),
+  winston.format.errors({ stack: true })
+);
+
+// Cria o logger
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        alignColorsAndTime
+      ),
+    }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+  ],
+});
 
 module.exports = logger;
