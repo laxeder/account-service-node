7f889fa18fcf208a6189a3b97c054673c7b8403e
laxeder <laxederbr@gmail.com>
2022-11-06 12:49:48 -0300

    resolving generate keys pair

--- src/infrastructure/shared/generate-keys-pair.js
+++ src/infrastructure/shared/generate-keys-pair.js
@@ -1,25 +1,15 @@
-const { RSA, Crypt } = require("hybrid-crypto-js");
-
-const { parseToJson } = require("../utils/parse");
-const handle = require("../utils/handle");
-
-const crypt = new Crypt({ md: "sha512" });
-const rsa = new RSA({ keySize: 4096 });
+const { AES: crypt } = require("crypto-js");
+const keypair = require("keypair");
 
 async function generateKeys() {
   try {
-    // ! extrai o par de chaves publica/privada RSA
-    const [err, { privateKey: private, publicKey: public }] = await handle(rsa.generateKeyPairAsync());
+    const { public, private } = keypair();
 
-    if (err) {
-      throw new Error(err);
-    }
+    const secret = crypt
+      .encrypt(public, "De@14@Jxfjm^MiKpQP6tzKSm83xpa*vfXRi2bSjvtSFGVbwU8Yy&9K*&soIT5ft&EIS")
+      .toString();
 
-    // ! gera uma chave IV de uma string forte de 64 caracteres e define a secret
-    const { iv: secret } = parseToJson(crypt.encrypt(public, "De@14@Jxfjm^MiKpQP6tzKSm83xpa*vfXRi2bSjvtSFGVbwU8Yy&9K*&soIT5ft&EIS"));
-
-    // ! gera uma chave de assinatura
-    const { signature } = parseToJson(await crypt.signature(private, secret));
+    const signature = crypt.encrypt(private, secret).toString();
 
     // ? returna o objeto de chaves
     return { public, private, secret, signature };
